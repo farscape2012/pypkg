d2965a2d72a314468cc89342c86f8ad4219acb07
farscape2012 <farscape2012@users.noreply.github.com>
2016-12-05 15:38:06 +0200

    Update table.py

--- lcy/table.py
+++ lcy/table.py
@@ -1,16 +1,15 @@
 import csv
 import pandas
 
-def write(data, file="output.csv", columns=None, index_in_list=False, mode="wb", sep=","):
-    #columns = ["abcd","bcde","cdef"] #a csv with 3 columns
+def write(data, file="output.csv", columns=None, index=False, index_in_list=False, mode="wb", sep=","):
     if index_in_list:
-        index = [i[0] for i in data] 
+        index_in_list = [i[0] for i in data] 
         data = [i[1:] for i in data]
     else:
-        index = False
-    df = pandas.DataFrame(data, columns=columns, index=index)
-    #Now you have a csv with columns and index:
-    df.to_csv(file, mode=mode, sep=sep)
+        index_in_list=None
+    
+    df = pandas.DataFrame(data, columns=columns, index=index_in_list)
+    df.to_csv(file, mode=mode, sep=sep, index=index)
 
         
 def read(file="input.csv", sep=",", header=None, names=None, index_col=None, usecols=None,comment=None):

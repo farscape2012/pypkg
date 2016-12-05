c2e0798a8282039ec57c275423b1667b888dd332
farscape2012 <farscape2012@users.noreply.github.com>
2016-12-05 15:53:10 +0200

    Update table.py

--- lcy/table.py
+++ lcy/table.py
@@ -1,15 +1,15 @@
 import csv
 import pandas
 
-def write(data, file="output.csv", columns=None, index=False, index_in_list=False, mode="wb", sep=","):
-    if index_in_list:
-        index_in_list = [i[0] for i in data] 
-        data = [i[1:] for i in data]
-    else:
-        index_in_list=None
-    
-    df = pandas.DataFrame(data, columns=columns, index=index_in_list)
-    df.to_csv(file, mode=mode, sep=sep, index=index)
+def write(data, file="output.csv", header=False, columns=None, index=False, index_in_list=False, mode="wb", sep=","):
+    if type(data) != pandas.core.frame.DataFrame:
+        if index_in_list:
+            index_in_list = [i[0] for i in data] 
+            data = [i[1:] for i in data]
+        else:
+            index_in_list=None
+        data = pandas.DataFrame(data, columns=columns, index=index_in_list)
+    data.to_csv(file, mode=mode, sep=sep, index=index, header=header)
 
         
 def read(file="input.csv", sep=",", header=None, names=None, index_col=None, usecols=None,comment=None):

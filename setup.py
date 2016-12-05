8ae0076f7fcd51c8e11998987d762cabb1e46449
farscape2012 <farscape2012@users.noreply.github.com>
2016-12-05 15:58:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
-
+from setuptools import setup
+setup(name='lcy',
+      version='0.1',
+      description='Functions of own house use',
+      url='https://github.com/farscape2012/pypkg',
+      author='Eric Chengyu Liu',
+      author_email='farscape2012@live.com',
+      license='MIT',
+      packages=['lcy'],
+      zip_safe=False)
